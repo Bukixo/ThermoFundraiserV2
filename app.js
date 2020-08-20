5a72810d9011e2ab323d5c5b405114d08fe43849
Buki Thompson <bukithompson@Bukis-MacBook-Air.local>
2020-08-20 08:06:15 +0100

    ui router setup and basic non-component routing works

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var app;
 (function (app) {
     "use strict";
-    angular.module("App", [
+    var appModule = angular.module("App", [
         "app.home",
         "app.navbar",
         "app.footer",
-        "app.crud"
+        "app.crud",
+        "ui.router"
     ]);
+    appModule.config(function ($stateProvider) {
+        var homeState = {
+            name: 'home',
+            url: '/home',
+            //   templateUrl: "src/thermostats/getThermostat/getThermostat.component.html"
+            template: "<h3>hello world!</h3>"
+        };
+        var dashboardState = {
+            name: 'dashboard',
+            url: '/dashboard',
+            //   templateUrl: "src/thermostats/getThermostat/getThermostat.component.html"
+            template: "<h3>hello dashboard!</h3>"
+        };
+        $stateProvider.state(homeState);
+        $stateProvider.state(dashboardState);
+    });
 })(app || (app = {}));
--- app.ts
+++ app.ts
@@ -1,10 +1,34 @@
 namespace app {
     "use strict";
 
-        angular.module("App", [
-            "app.home",
-            "app.navbar",
-            "app.footer",
-            "app.crud"
-        ])
+    var appModule = angular.module("App", [
+        "app.home",
+        "app.navbar",
+        "app.footer",
+        "app.crud",
+        "ui.router"
+    ])
+
+
+    appModule.config(function($stateProvider) {
+        var homeState = {
+          name: 'home',
+          url: '/home',
+        //   templateUrl: "src/thermostats/getThermostat/getThermostat.component.html"
+        template: "<h3>hello world!</h3>"
+        }
+      
+        var dashboardState = {
+          name: 'dashboard',
+          url: '/dashboard',
+        //   templateUrl: "src/thermostats/getThermostat/getThermostat.component.html"
+        template: "<h3>hello dashboard!</h3>"
+        }
+      
+        $stateProvider.state(homeState);
+        $stateProvider.state(dashboardState);
+      });
+
+
+
 }
